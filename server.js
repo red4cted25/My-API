9acb2fd8477f6f87c9ed95efaf64a68420708b48
Nico <141285419+red4cted25@users.noreply.github.com>
2024-10-01 15:25:38 -0700

    Converted Project to HTML
    Removed all EJS functionality and converted the entire project to HTML functionality.
    ----------------ISSUES----------------
    Issues using res.render and loading pages, all functionality not complete yet, but some framework is there for functionality and future pages.

--- server.js
+++ server.js
@@ -2,21 +2,81 @@ var express = require('express')
 var app = express()
 const fs = require('fs')
 const PORT = 5000
-const bodyParser = require('body-parser')
-const expressLayouts = require('express-ejs-layouts');
-const path = require('path');
+const path = require('path')
 
-// Middleware
-app.use(expressLayouts);
-app.set('layout', 'layouts/main');
-app.use('/public', express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('/public'));
 
-app.set('view engine', 'ejs')
-
+// Index
 app.get('/', (req, res) => {
-    res.render('pages/index', {
-        pageTitle: 'Home',
-        customStylesheet: '/public/css/index.css'
-    })
+    res.status(300).redirect('/api')
 })
+
+app.get('/api', (req, res) => {
+    res.send('/public/index.html')
+})
+
+app.get('/api/movies', (req, res) => {
+    let movies = getMovies();
+    res.json(movies);
+})
+
+app.get('/api/directors', (req, res) => {
+    res.render('index.html')
+})
+
+// ! ADMIN
+app.get('/add/movie/:title/:year/:genre', async (req, res) => {
+    try {
+        const { title, year, genre } = req.params;
+        let movies = [];
+        try {
+            const data = fs.readFile(path.join(__dirname + '/data/movies.json'), 'utf8');
+            movies = JSON.parse(data);
+        } catch (error) {
+            console.error(error);
+        }
+
+        // Find highest existing movie ID
+        const highestId = movies.reduce((max, movie) => {
+            Math.max(max, movie.id || 0)
+        }, 0)
+
+        const newMovie = {
+            id: highestId + 1,
+            title,
+            year,
+            genre,
+        }
+        movies.push(newMovie);
+        fs.writeFile(path.join(__dirname, 'movies.json'), JSON.stringify(movies, null, 2 ))
+        res.status(201).json({ message: 'Movie added successfully', movie: newMovie });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error adding movie');
+    }
+})
+
+// ! FUNCTIONS
+async function getMovies() {
+    try {
+        const data = fs.readFileSync(path.join(__dirname + '/data/movies.json'), 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+async function getDirectors() {
+    try {
+        const data = fs.readFile(path.join(__dirname + '/data/directors.json'), 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
